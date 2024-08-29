b31b8bd3f6711a2afb8488254d4376331da38d87
dmitryhatsayuk <155708514+dmitryhatsayuk@users.noreply.github.com>
2024-08-29 12:50:27 +0700

    added methods

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,2 +1,111 @@
-package org.example;public class Server {
+package org.example;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+    ExecutorService threadPool = Executors.newFixedThreadPool(64);
+
+    public void listen(int port) {
+        int th = 0;
+        try (final var serverSocket = new ServerSocket(port)) {
+            while (true) {
+                final var socket = serverSocket.accept();
+                Runnable logic = () -> {
+                    try {
+                        processing(socket);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                };
+                threadPool.submit(logic);
+                System.out.println("Thread added " + th);
+//Добавил счетчик потоков, чтобы понять сколько новых запустится и получается при обновлении страницы
+                //каждый раз новое подключение и поток, это меня не удовлетворило, кажется так не должно быть
+                th++;
+            }
+
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void processing(Socket socket) throws IOException {
+
+        try (
+                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                final var out = new BufferedOutputStream(socket.getOutputStream())
+        ) {
+            while (true) {
+                // read only request line for simplicity
+                // must be in form GET /path HTTP/1.1
+                final var requestLine = in.readLine();
+                final var parts = requestLine.split(" ");
+
+                if (parts.length != 3) {
+                    // just close socket
+                    continue;
+                }
+
+                final var path = parts[1];
+                if (!validPaths.contains(path)) {
+                    out.write((
+                            "HTTP/1.1 404 Not Found\r\n" +
+                                    "Content-Length: 0\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    out.flush();
+                    continue;
+                }
+
+                final var filePath = Path.of(".", "public", path);
+                final var mimeType = Files.probeContentType(filePath);
+
+                // special case for classic
+                if (path.equals("/classic.html")) {
+                    final var template = Files.readString(filePath);
+                    final var content = template.replace(
+                            "{time}",
+                            LocalDateTime.now().toString()
+                    ).getBytes();
+                    out.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + mimeType + "\r\n" +
+                                    "Content-Length: " + content.length + "\r\n" +
+                                    "Connection: keep-alive\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    out.write(content);
+                    out.flush();
+                    continue;
+                }
+
+                final var length = Files.size(filePath);
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: keep-alive\r\n" +
+                                //добавил keep alive но соединение все равно рвется пока не понял почему
+                                "\r\n"
+                ).getBytes());
+                Files.copy(filePath, out);
+                out.flush();
+            }
+        }
+        //конец цикла
+    }
 }
